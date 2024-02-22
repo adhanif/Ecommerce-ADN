24bf5c7f001a36fc1312acf53ad742f431da8c99
ad.hanif <ad.hanif@outlook.com>
2024-02-22 14:45:50 +0200

    sorting order by price and pagination implemented

--- src/components/product/ProductsDataFetch.tsx
+++ src/components/product/ProductsDataFetch.tsx
@@ -1,15 +1,57 @@
-import React, { useCallback, useEffect, useMemo } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 
-import { Box, Container, Grid, Typography } from '@mui/material';
+import {
+  Box,
+  Container,
+  FormControl,
+  FormHelperText,
+  Grid,
+  MenuItem,
+  Pagination,
+  Select,
+  SelectChangeEvent,
+  Typography,
+} from '@mui/material';
 import { styled } from '@mui/system';
 import { useFetchAllProductsQuery } from '../../redux/productsQuery';
 import ProductCard from './ProductCard';
 import Loading from '../loading/Loading';
+import { Product } from '../../misc/types';
 
 export default function ProductsDataFetch() {
   const { data, error, isLoading } = useFetchAllProductsQuery();
+  const [sortBy, setSortBy] = useState('');
 
-  const memoizedData = useMemo(() => data, [data]);
+  // Pagination logic
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 12;
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+
+  const memoizedData = useMemo(() => {
+    if (!data) return [];
+    if (sortBy === 'lowToHigh') {
+      return [...data].sort((a, b) => a.price - b.price);
+    } else if (sortBy === 'highToLow') {
+      return [...data].sort((a, b) => b.price - a.price);
+    } else {
+      return data;
+    }
+  }, [data, sortBy]);
+
+  const slicedData = memoizedData.slice(startIndex, endIndex);
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setSortBy(event.target.value);
+  };
+
+  const handlePageChange = (
+    event: React.ChangeEvent<unknown>,
+    page: number,
+  ) => {
+    setCurrentPage(page);
+  };
 
   if (isLoading) {
     return (
@@ -21,60 +63,89 @@ export default function ProductsDataFetch() {
 
   return (
     <>
-      <>
-        <Box
-          id='highlights'
+      <Box
+        id='highlights'
+        sx={{
+          pb: { xs: 8, sm: 16 },
+        }}
+      >
+        <Container
           sx={{
-            pb: { xs: 8, sm: 16 },
+            position: 'relative',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            gap: { xs: 3, sm: 6 },
           }}
         >
-          <Container
+          <Box
             sx={{
-              position: 'relative',
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              gap: { xs: 3, sm: 6 },
+              width: { sm: '100%', md: '60%' },
+              textAlign: { sm: 'left', md: 'center' },
             }}
           >
-            <Box
-              sx={{
-                width: { sm: '100%', md: '60%' },
-                textAlign: { sm: 'left', md: 'center' },
-              }}
-            >
-              <Typography component='h2' variant='h4'>
-                Products
-              </Typography>
-              <Typography variant='body1'>
-                Explore why our product stands out: adaptability, durability,
-                user-friendly design, and innovation. Enjoy reliable customer
-                support and precision in every detail.
-              </Typography>
-            </Box>
-            <Grid
-              container
-              spacing={2.5}
-              // gap={1}
-            >
-              {memoizedData &&
-                memoizedData.map((product, index) => (
-                  <Grid
-                    item
-                    xs={12}
-                    sm={6}
-                    md={4}
-                    lg={3}
-                    key={index}
-                    rowGap={5}
-                  >
-                    <ProductCard product={product} />
-                  </Grid>
-                ))}
+            <Typography component='h2' variant='h4'>
+              Products
+            </Typography>
+            <Typography variant='body1'>
+              Explore why our product stands out: adaptability, durability,
+              user-friendly design, and innovation. Enjoy reliable customer
+              support and precision in every detail.
+            </Typography>
+          </Box>
+
+          <Grid
+            container
+            display='flex'
+            justifyContent='space-between'
+            alignItems='center'
+            marginBottom='3rem'
+          >
+            <Grid item>
+              <Typography>{`Showing all ${memoizedData.length} results`}</Typography>
             </Grid>
-          </Container>
-        </Box>
-      </>
+            <Grid item>
+              <FormControl sx={{ minWidth: 230, border: 'none' }} size='small'>
+                <Select
+                  value={sortBy}
+                  onChange={handleChange}
+                  displayEmpty
+                  inputProps={{ 'aria-label': 'Without label' }}
+                >
+                  <MenuItem value=''>Default sorting</MenuItem>
+                  <MenuItem value='lowToHigh'>
+                    Sort by price: low to high
+                  </MenuItem>
+                  <MenuItem value='highToLow'>
+                    Sort by price: high to low
+                  </MenuItem>
+                  {/* <MenuItem value={30}>Thirty</MenuItem> */}
+                </Select>
+              </FormControl>
+            </Grid>
+          </Grid>
+
+          <Grid
+            container
+            spacing={2.5}
+            // gap={1}
+          >
+            {slicedData &&
+              slicedData.map((product, index) => (
+                <Grid item xs={12} sm={6} md={4} lg={3} key={index} rowGap={5}>
+                  <ProductCard product={product} />
+                </Grid>
+              ))}
+          </Grid>
+          <Box marginTop='5rem'>
+            <Pagination
+              count={Math.ceil(memoizedData.length / itemsPerPage)}
+              page={currentPage}
+              onChange={handlePageChange}
+            />
+          </Box>
+        </Container>
+      </Box>
     </>
   );
 }
